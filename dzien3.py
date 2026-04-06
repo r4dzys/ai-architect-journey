2c5da1d13e27951f77f04806b810afe1d6a12b6b
r4dzys <r4dzys@gmail.com>
2026-04-06 22:17:43 +0000

    Day 3 - Lists, dictionaries and 'for' loops

---
+++ dzien3.py
@@ -0,0 +1,11 @@
+
+FAQ = {
+    "question1":"answer1",
+    "question2":"answer2",
+    "question3":"answer3",
+    "question4":"answer4",
+    "question5":"answer5"
+}
+
+for q in FAQ:
+  print(q, FAQ[q], sep='? - ')
